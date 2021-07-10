dcaefa1fc7ed94213b501e48eac78604a36f9712
Lucas Moraes <lucas.anjos.moraes@gmail.com>
2021-07-10 18:28:23 -0300

    feat: create funcs to manage correlation ID on services

---
+++ pkg/toolkit/correlationID.go
@@ -0,0 +1,40 @@
+package toolkit
+
+import (
+	"context"
+
+	"fake-hash.ports-adapters/pkg/http"
+)
+
+const (
+	CorrelationKey       = "correlationID"
+	CorrelationHeaderKey = "x-correlation-id"
+)
+
+// LoadCorrelationID procura pelo header relacionado ao correlation ID na
+// requisição HTTP dada e o carrega no contexto dado.
+func LoadCorrelationID(ctx context.Context, req http.Request) context.Context {
+	prev := ExtractCorrelationID(ctx)
+	correlationID := req.Header(CorrelationHeaderKey)
+
+	if correlationID == "" && prev != "" {
+		correlationID = prev
+	}
+
+	return context.WithValue(ctx, CorrelationKey, correlationID)
+}
+
+// ExtractCorrelationID irá procurar pelo correlation ID no contexto dado.
+func ExtractCorrelationID(ctx context.Context) string {
+	v := ctx.Value(CorrelationKey)
+	if v == nil {
+		return ""
+	}
+
+	c, ok := v.(string)
+	if ok {
+		return c
+	}
+
+	return ""
+}
